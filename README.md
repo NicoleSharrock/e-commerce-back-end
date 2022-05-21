cf99d14f12511b2baeaf76a3f2af5c01428095bf
NicoleSharrock <n.sharrockhayes@gmail.com>
2022-05-21 07:54:07 -0400

    updated files

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # E-commerce Back End Starter Code
+
+
+## Purpose 
+ 
+
+## Table Of Contents
+* [General info](#story)
+* [Technologies](#built-with)
+* [Deployable link](#deployable-link) 
+* [Screenshot](#screenshot)
+
+## Story
+As a manager at an internet retail company I want a back end for my e-commerce website that uses the latest technologies so that my company can compete with other e-commerce companies.
+
+
+
+
+## Built with
+* Javascript 
+* Node
+* MySQL2
+* Inquirer
+* Express
+* Sequelize
+
+
+## Walkthrough video
+
+
+
+## Screenshot:
+![screenshot]()
--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  forgeignKey: "category_id"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  forgeignkey: "category_id"
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  forgeignKey: "product_id"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  forgeignKey: "tag_id"
+});
 
 module.exports = {
   Product,
